df837c3d522eeafd1f623055e164083a1acf19c7
Paolo Gomez <peweegardner248@gmail.com>
2022-04-26 10:49:58 -0700

    Update app.js

--- api/app.js
+++ api/app.js
@@ -16,6 +16,9 @@ const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'tr
 // create the Express app
 const app = express();
 
+// Cross-origin resource sharing
+
+
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
 
