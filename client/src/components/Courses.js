f3f9e605b43b98fe7d5d0160178ecf8e22ddec1f
Paolo Gomez <peweegardner248@gmail.com>
2022-04-27 10:19:59 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 // provides the "Courses" screen
 // retrieve list of courses from /api/courses
 //render list of courses
+// link to respective "Course Detail" screen
 
 
 export default Courses
