35814ae49eb2d338dd02cadf454d5c54ad63f846
Paolo Gomez <peweegardner248@gmail.com>
2022-04-28 11:01:50 -0700

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 
 // provides the "Course Detail" screen
 // retrieve detail fro course from /api/courses/:id
+// render "Delete Course" button
 
 export default CourseDetails
