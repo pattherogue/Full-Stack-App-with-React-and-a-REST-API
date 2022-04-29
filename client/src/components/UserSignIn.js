938e6cd42af352333a025cfbe729d95576c28734
Paolo Gomez <peweegardner248@gmail.com>
2022-04-29 10:54:08 -0700

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -3,5 +3,6 @@ import React, { Component } from 'react';
 // provide "Sign In" screen
 // user sign in w/ exisiting account information
 // render "Sign In" button 
+// render "Cancel" button 
 
 export default UserSignIn
