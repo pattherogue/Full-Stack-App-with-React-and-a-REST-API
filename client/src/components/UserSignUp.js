b16e1098c888c9fd5a01cf750efd943c7cb87b23
Paolo Gomez <peweegardner248@gmail.com>
2022-04-30 08:11:00 -0700

    Update UserSignUp.js

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -3,5 +3,6 @@ import React, { Component } from 'react';
 // provides "Sign Up" screen 
 // render form - allow user to sign up
 // render "Sign Up" button
+// cick --> sends a POST request 
 
 export default UserSignUp
