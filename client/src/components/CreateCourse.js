0e5e97ff93c381d6f10ad24308a76e9f1b202082
Paolo Gomez <peweegardner248@gmail.com>
2022-05-01 14:12:35 -0700

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -2,5 +2,7 @@ import React, { Comppnent } from 'react';
 
 // provides "Create Course" screen
 // render screen that allows user to create a new course
+// render "Create Course" button
+
 
 export default CreateCourse
