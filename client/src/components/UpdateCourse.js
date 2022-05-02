7218515d9e3f0882a286866f8fde3bd7e43f5815
Paolo Gomez <peweegardner248@gmail.com>
2022-05-02 12:58:39 -0700

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -3,3 +3,4 @@ import React, { Component } from 'react';
 // provides "Update Course" screen
 // render form -- update one existing course
 // render "Update Course" button
+// send PUT request to /api/courses/:id
