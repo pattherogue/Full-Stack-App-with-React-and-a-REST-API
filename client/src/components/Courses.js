e524046d2b19fcc5a820a6bcace407f4f17779ab
Paolo Gomez <peweegardner248@gmail.com>
2022-05-06 11:30:52 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -27,7 +27,6 @@ class Courses extends Component {
         const coursesList = courses.map((course) => {
             
             return (
-                <div className="wrap main--grid">
                     <a class="course--module course--link" href="course-detail.html">
                         <h2 class="course--label">Course</h2>
                         <h3 class="course--title">Build a Basic Bookcase</h3>
