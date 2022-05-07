7b373da0e6028c15768b30325064de40581b97bf
Paolo Gomez <peweegardner248@gmail.com>
2022-05-07 11:44:39 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -27,27 +27,27 @@ class Courses extends Component {
         const coursesList = courses.map((course) => {
             
             return (
-                    <a key={course.id} className="course--module course--link" href={`/courses/${course.id}`}>
-                        <h2 className="course--label">Course</h2>
-                        <h3 className="course--title">Build a Basic Bookcase</h3>
-                    </a>
+                <a key={course.id} className="course--module course--link" href={`/courses/${course.id}`}>
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Build a Basic Bookcase</h3>
+                </a>
             )
         });
 
        
             return (
-                
+                <a class="course--module course--add--module" href="create-course.html">
+                <span class="course--add--title">
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                    New Course
+                </span>
+            </a>
             ) 
 
 
 
-                <a class="course--module course--add--module" href="create-course.html">
-                    <span class="course--add--title">
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                        viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-                        New Course
-                    </span>
-                </a>
+          
           
         
     }
