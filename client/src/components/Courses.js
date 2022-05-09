95c1a7ddc4924c6434ba000798c5191b374d7683
Paolo Gomez <peweegardner248@gmail.com>
2022-05-09 12:06:00 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -29,7 +29,7 @@ class Courses extends Component {
             return (
                 <a key={course.id} className="course--module course--link" href={`/courses/${course.id}`}>
                     <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Build a Basic Bookcase</h3>
+                    <h3 className="course--title">{course.title}</h3>
                 </a>
             )
         });
