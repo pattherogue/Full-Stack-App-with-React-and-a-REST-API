403981c416729ae23379704a01e4981208ac9559
Paolo Gomez <peweegardner248@gmail.com>
2022-05-19 11:46:52 -0700

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -66,17 +66,7 @@ class CourseDetail extends Component {
 
                     <h3 className="course--detail--title">Materials Needed</h3>
                     <ul className="course--detail--list">
-                        <li>1/2 x 3/4 inch parting strip</li>
-                        <li>1 x 2 common pine</li>
-                        <li>1 x 4 common pine</li>
-                        <li>1 x 10 common pine</li>
-                        <li>1/4 inch thick lauan plywood</li>
-                        <li>Finishing Nails</li>
-                        <li>Sandpaper</li>
-                        <li>Wood Glue</li>
-                        <li>Wood Filler</li>
-                        <li>Minwax Oil Based Polyurethane</li>
-                    </ul>
+                  
                 </div>
             </div>
         </form>
