f658d06309558f5b667e68672385a92d2a26d31e
Paolo Gomez <peweegardner248@gmail.com>
2022-05-20 10:15:14 -0700

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -40,7 +40,7 @@ class CourseDetail extends Component {
 
     render() {
         return (
-            <div class="actions--bar">
+            <><div class="actions--bar">
                 <div class="wrap">
                     <React.Fragment>
                         <a className="button" href={`/courses/${this.state.id}/update`}>Update Course</a>
@@ -48,34 +48,31 @@ class CourseDetail extends Component {
                         <a className="button button-secondary" href="/">Return to List</a>
                     </React.Fragment>
                 </div>
-            </div>
-
-            <div className="wrap">
-            <h2>Course Detail</h2>
-
-            <form>
-                <div className="main--flex">
-                    <div>
-                        <h3 className="course--detail--title">Course</h3>
-                        <h4 className="course--name">{this.state.title}</h4>
-                        <p>By {this.state.firstName} {this.state.lastName}</p>
-                        <p> {this.state.description} </p>
-                    </div>
-                <div>
-                            <h3 className="course--detail--title">Estimated Time</h3>
-                            <p> {this.state.estimatedTime} </p>
+            </div><div className="wrap">
+                    <h2>Course Detail</h2>
+                    <form>
+                        <div className="main--flex">
+                            <div>
+                                <h3 className="course--detail--title">Course</h3>
+                                <h4 className="course--name">{this.state.title}</h4>
+                                <p>By {this.state.firstName} {this.state.lastName}</p>
+                                <p> {this.state.description} </p>
+                            </div>
+                            <div>
+                                <h3 className="course--detail--title">Estimated Time</h3>
+                                <p> {this.state.estimatedTime} </p>
                                 <ReactMarkdown>
                                     {this.state.estimatedTime}
                                 </ReactMarkdown>
-                            <h3 className="course--detail--title">Materials Needed</h3>
+                                <h3 className="course--detail--title">Materials Needed</h3>
                                 <ReactMarkdown>
                                     {this.state.materialsNeeded}
-                                <ReactMarkdown>
-                            
-                        </div>
-                    </div>
-            </form>
-            </div>
+                                    <ReactMarkdown>
+
+                                    </div>
+                                </></div>
+                        </></form>
+                </div></>
         )
     }
 
