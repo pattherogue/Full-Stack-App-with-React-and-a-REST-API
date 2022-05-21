bb26a9e56254f9ce0aae63dfc5de62e148de326b
Paolo Gomez <peweegardner248@gmail.com>
2022-05-21 10:56:43 -0700

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+class UserSignIn extends Commponent {
+    
+}
 // provide "Sign In" screen
 // user sign in w/ exisiting account information
 // render "Sign In" button 
