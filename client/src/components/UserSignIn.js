b17e8da553f435d584aca81a15b4505b36e6b41d
Paolo Gomez <peweegardner248@gmail.com>
2022-05-22 11:58:51 -0700

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -13,11 +13,26 @@ class UserSignIn extends Commponent {
             password,
             errors
         } = this.state;
+
+        return(
+            <div class="form--centered">
+                <h2>Sign In</h2>
+                
+                <form>
+                    <label for="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" value="">
+                    <label for="password">Password</label>
+                    <input id="password" name="password" type="password" value="">
+                    <button class="button" type="submit">Sign In</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                </form>
+                <p>Don't have a user account? Click here to <a href="sign-up.html">sign up</a>!</p>
+                
+            </div>
+        )
+    
 }
 
-return(
-    
-)
+
 // provide "Sign In" screen
 // user sign in w/ exisiting account information
 // render "Sign In" button 
