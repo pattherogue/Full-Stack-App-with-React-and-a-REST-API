3bf2d3aac73a0f75352fadad6a79ff944b519f12
Paolo Gomez <peweegardner248@gmail.com>
2022-05-25 09:28:54 -0700

    update

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -45,10 +45,7 @@ class UserSignIn extends Commponent {
                         </React.Fragment>
                     )}
                 />
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="">
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value="">
+
                     <button class="button" type="submit">Sign In</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
                 </form>
                 <p>Don't have a user account? Click here to <a href="sign-up.html">sign up</a>!</p>
