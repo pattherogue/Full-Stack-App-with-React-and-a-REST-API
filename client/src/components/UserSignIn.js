68a88085ea287331e271b23283929097ce6e0fac
Paolo Gomez <peweegardner248@gmail.com>
2022-05-27 09:16:56 -0700

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -68,7 +68,7 @@ class UserSignIn extends Commponent {
         const { from } = this.props.location.state;
         const { emailAddress, password } = this.state;
 
-        context.actions.signIn()
+        context.actions.signIn(emailAddress, password)
     }
 
 
