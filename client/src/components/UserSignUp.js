1a6254251c3b0a77aa5cd5a156bb2690743ead0d
Paolo Gomez <peweegardner248@gmail.com>
2022-06-04 09:06:56 -0700

    Update UserSignUp.js

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -57,16 +57,7 @@ class UserSignUp extends Component {
                     </React.Fragment>
                 )}
             >
-                <label for="firstName">First Name</label>
-                <input id="firstName" name="firstName" type="text" value="">
-                <label for="lastName">Last Name</label>
-                <input id="lastName" name="lastName" type="text" value="">
-                <label for="emailAddress">Email Address</label>
-                <input id="emailAddress" name="emailAddress" type="email" value="">
-                <label for="password">Password</label>
-                <input id="password" name="password" type="password" value="">
-                <button class="button" type="submit">Sign Up</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
-           
+                
             <p>Already have a user account? Click here to <a href="sign-in.html">sign in</a>!</p>
         </div>
         )
