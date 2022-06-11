87aba4ec2c69964036fac89f4103cff2df0a2a61
Paolo Gomez <peweegardner248@gmail.com>
2022-06-11 08:52:20 -0700

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -5,6 +5,7 @@ class CreateCourse extends Comppnent {
         title: '',
         description: '',
         estimatedTime: '',
+        materialsNeeded: '',
     }
 }
 // provides "Create Course" screen
