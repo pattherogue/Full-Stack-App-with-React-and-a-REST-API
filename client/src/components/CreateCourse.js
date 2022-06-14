742509bbfee6dee8a6cefdeb47efaba76cc1cafb
Paolo Gomez <peweegardner248@gmail.com>
2022-06-14 12:59:20 -0700

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -28,24 +28,24 @@ class CreateCourse extends Comppnent {
                 submitButtonText="Create Course"
                 elements={() => (
                     <React.Fragment>
-                         <div class="main--flex">
-                    <div>
-                        <label for="courseTitle">Course Title</label>
-                        <input id="courseTitle" name="courseTitle" type="text" value="">
+                        <div class="main--flex">
+                            <div>
+                                <label for="courseTitle">Course Title</label>
+                                <input id="courseTitle" name="courseTitle" type="text" value="">
 
-                        <p>By Joe Smith</p>
+                                <p>By Joe Smith</p>
 
-                        <label for="courseDescription">Course Description</label>
-                        <textarea id="courseDescription" name="courseDescription"></textarea>
-                    </div>
-                    <div>
-                        <label for="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value="">
+                                <label for="courseDescription">Course Description</label>
+                                <textarea id="courseDescription" name="courseDescription"></textarea>
+                            </div>
+                            <div>
+                                <label for="estimatedTime">Estimated Time</label>
+                                <input id="estimatedTime" name="estimatedTime" type="text" value="">
 
-                        <label for="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
-                    </div>
-                </div>
+                                <label for="materialsNeeded">Materials Needed</label>
+                                <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+                            </div>
+                        </div>
                     </React.Fragment>
                 )}
             
