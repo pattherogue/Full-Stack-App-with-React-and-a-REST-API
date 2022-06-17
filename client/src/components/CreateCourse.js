0de00ed53ac0c63b014d1a365f61474709bf81ff
Paolo Gomez <peweegardner248@gmail.com>
2022-06-17 08:48:14 -0700

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -71,33 +71,12 @@ class CreateCourse extends Comppnent {
                         </div>
                     </React.Fragment>
                 )}
-            
             />
-            
-            
-            <form>
-                <div class="main--flex">
-                    <div>
-                        <label for="courseTitle">Course Title</label>
-                        <input id="courseTitle" name="courseTitle" type="text" value="">
-
-                        <p>By Joe Smith</p>
-
-                        <label for="courseDescription">Course Description</label>
-                        <textarea id="courseDescription" name="courseDescription"></textarea>
-                    </div>
-                    <div>
-                        <label for="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value="">
-
-                        <label for="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
-                    </div>
-                </div>
-                <button class="button" type="submit">Create Course</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
-            </form>
         </div>
-        )
+        );
+                }    
+            
+    
     }
 }
 // provides "Create Course" screen
