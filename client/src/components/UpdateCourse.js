cad6a616d0bcb436a1a09696ac1822e7193989bf
Paolo Gomez <peweegardner248@gmail.com>
2022-06-25 08:51:13 -0700

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -28,6 +28,7 @@ export default class UpdateCourse extends Component {
                         cancel={this.cancel}
                         errors={errors}
                         submit={this.submit}
+                        submitButtonText="Update Course"
 
                     >
                         <div class="main--flex">
