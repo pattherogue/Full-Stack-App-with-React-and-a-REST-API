9a572d8771c0ae37a3fba3b9fae6e0015b4baf44
Paolo Gomez <peweegardner248@gmail.com>
2022-07-02 12:12:03 -0700

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 
+// provides "Update Course" screen
+
+// render "Update Course" button
+// send PUT request to /api/courses/:id
+// render "Cancel" button
+// return users to "Course Detail" screen
+
 export default class UpdateCourse extends Component {
     state = {
         title: '',
@@ -10,7 +17,7 @@ export default class UpdateCourse extends Component {
         id: this.props.match.params.id,
         errors: [],
     }
-
+    // render form -- update one existing course
     render() {
         const {
             title,
@@ -138,9 +145,4 @@ export default class UpdateCourse extends Component {
         this.props.history.push('/');
     }
 }
-// provides "Update Course" screen
-// render form -- update one existing course
-// render "Update Course" button
-// send PUT request to /api/courses/:id
-// render "Cancel" button
-// return users to "Course Detail" screen
+
