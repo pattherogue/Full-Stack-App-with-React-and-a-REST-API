d10e747568fed340fbdd40d634458a346d989c22
Paolo Gomez <peweegardner248@gmail.com>
2022-07-04 09:30:56 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default class Header extends Componenet {
     render() {
         const { context } = this.props;
+        const authUser = context.authenticatedUser;
     }
 }
 // display top menu bar
