6c415a16127a5a1ddc6c71f99121fd294117cba6
Paolo Gomez <peweegardner248@gmail.com>
2022-07-04 09:29:37 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Componenet } from 'react';
 import { NavLink } from 'react-router-dom';
 
-
+export default class Header extends Componenet {
+    
+}
 // display top menu bar
 // sign in button
 // sign up button ( if not authenticated user)
