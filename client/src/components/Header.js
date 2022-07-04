96bdf849c9817feb29f7a3d9b13aee27d64a8600
Paolo Gomez <peweegardner248@gmail.com>
2022-07-04 09:23:59 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 // display top menu bar
 // sign in button
-// sign up button
+// sign up button ( if not authenticated user)
 // user's name 
-// sign out button
+// sign out button (if authenticated user)
