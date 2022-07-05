89d2752aee86aa3202b452c5b3d77868bd3fac68
Paolo Gomez <peweegardner248@gmail.com>
2022-07-05 10:30:41 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,17 @@ export default class Header extends Componenet {
         const authUser = context.authenticatedUser;
 
         return (
-            
+            <header>
+                <div class="wrap header--flex">
+                    <h1 class="header--logo"><a href="index.html">Courses</a></h1>
+                    <nav>
+                        <ul class="header--signedout">
+                            <li><a href="sign-up.html">Sign Up</a></li>
+                            <li><a href="sign-in.html">Sign In</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
         )
     }
 }
