e133c1faf41c8d4d3314df9b5c154a5af5d79bd6
Paolo Gomez <peweegardner248@gmail.com>
2022-07-08 07:52:52 -0700

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown'
 
 class CourseDetail extends Component {
