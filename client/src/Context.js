88e14c05a869b447ff394d05d88729c7d6c1c940
Paolo Gomez <peweegardner248@gmail.com>
2022-07-15 08:59:53 -0700

    Update Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -39,8 +39,8 @@ export class Provider extends Component {
     signIn = async (emailAddress, password) => {
         const user = await this.data.getUser(emailAddress, password);
 
-        if () {
-            
+        if (user !== null) {
+
         }
     }
 
