b2488d092cd0fd164fd5b9c4e824b27b034479be
Paolo Gomez <peweegardner248@gmail.com>
2022-07-17 12:06:04 -0700

    Update Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -64,7 +64,7 @@ export class Provider extends Component {
 
 export const Consumer = Context.Consumer;
 
-export default function withContext(() {
-    
+export default function withContext((Component) {
+
 })
 
