8316d5cfb5940607d8cc8d1f883ccf6c13b04941
Paolo Gomez <peweegardner248@gmail.com>
2022-07-21 08:24:32 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,7 @@ const App = () => (
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
         <Route path="/error" component={UnhandledError} />
+        <Route path="/notfound"
       </Switch>
   </Router>
 
