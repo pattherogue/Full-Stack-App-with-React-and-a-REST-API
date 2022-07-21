8fd7d1c56d678eb5f346aeffea8e9ce94b5fb53d
Paolo Gomez <peweegardner248@gmail.com>
2022-07-21 08:24:05 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ const App = () => (
         <Route path="/courses/:id" component={CourseDetailWtihContext} />
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signout" component={UserSignOutWithContext} />
-        <Route path="/error" component={}
+        <Route path="/error" component={UnhandledError} />
       </Switch>
   </Router>
 
