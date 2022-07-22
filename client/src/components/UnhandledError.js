680bfe5e2bc1ffe95c2a600c090a1d94003040db
Paolo Gomez <peweegardner248@gmail.com>
2022-07-22 08:54:10 -0700

    Update UnhandledError.js

--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function UnhandledError() {
     return (
         <div className="wrap">
-            <h2></h2>
+            <h2>Error</h2>
         </div>
     )
 }
