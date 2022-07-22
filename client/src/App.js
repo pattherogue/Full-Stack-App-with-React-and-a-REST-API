3a667f3106705f211a67d5d57c3af3b45f22fdb7
Paolo Gomez <peweegardner248@gmail.com>
2022-07-22 08:46:34 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 
 import withCOntext from "./Context";
+import PrivateRoute from "./PrivateRoute";
 
 const HeaderWithContext = HeaderWithContext(Header);
 const CourseWithContext = withContext(Course);
