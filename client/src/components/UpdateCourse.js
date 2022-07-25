2e72a5503bad044d3b08e50039329d6a6aa3ffd0
Paolo Gomez <peweegardner248@gmail.com>
2022-07-25 09:41:21 -0700

    Update UpdateCourse.js

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -15,7 +15,7 @@ export default class UpdateCourse extends Component {
     async componentDidMount() {
         await this.getCourse().catch(err => {
             console.log(err);
-        })
+        });
     }
     // render form -- update one existing course
     render() {
