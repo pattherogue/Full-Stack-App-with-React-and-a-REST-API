d5381320e94931585345e081da188715b53046f8
Paolo Gomez <peweegardner248@gmail.com>
2022-08-03 09:30:56 -0700

    Update Data.js

--- client/src/Data.js
+++ client/src/Data.js
@@ -1,5 +1,9 @@
 export default class Data {
     api(path, method = 'GET', body = null, requiresAuth = false, credentials = null) {
         const url = config.apiBaeUrl + path;
+
+        const options = {
+            
+        }
     }
 }
