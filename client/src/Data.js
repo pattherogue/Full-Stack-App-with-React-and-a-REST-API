6a3d4782b9daa036b150de48d9f3f25f91586fda
Paolo Gomez <peweegardner248@gmail.com>
2022-08-05 08:42:01 -0700

    Update Data.js

--- client/src/Data.js
+++ client/src/Data.js
@@ -16,6 +16,8 @@ export default class Data {
         if (requiresAuth) {
             const encodedCredentials = bota(`${credentials.emailAddress}:${credentials.password}`);
             options.headers['Authorization'] = `Basic ${encodedCredentials}`;
-        };
+        }
+
+        return fetch(url, options);
     }
 }
