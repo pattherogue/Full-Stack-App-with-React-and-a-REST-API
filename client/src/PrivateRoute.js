53de96055d35d1b8ea85404d0020a68485ca9702
Paolo Gomez <peweegardner248@gmail.com>
2022-08-08 12:25:13 -0700

    Update PrivateRoute.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -3,5 +3,13 @@ import { Route, Redirect } from 'react-router-dom';
 import { Consumer } from './Contet';
 
 const privateRoute = ({ component: Component, ...rest }) => {
-    
+    return (
+        <Consumer>
+            {context => (
+
+            ) : (
+                
+            )}
+        </Consumer>
+    )
 }
