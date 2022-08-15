b152f3ae7297d7c385db9613e14faf043cabebff
Paolo Gomez <peweegardner248@gmail.com>
2022-08-15 09:19:20 -0700

    Update Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -9,7 +9,7 @@ export class Provider extends Component {
     constructor() {
         super();
         this.data = new Data();
-        this.cookie = Cookies.get('authenticate4dUser');
+        this.cookie = Cookies.get('authenticatedUser');
 
         this.state = {
             authenticatedUser: this.cookie ? JSON.parse(this.cookie) : null
