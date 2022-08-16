4c74337a9020b0ffbb8ef4a557f088595c151edf
Paolo Gomez <peweegardner248@gmail.com>
2022-08-15 18:26:23 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from './Context';
 
+import './styles/reset.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider>
