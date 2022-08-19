88bb743f0af0db74648698fab2bca7b7aa360ec2
Paolo Gomez <peweegardner248@gmail.com>
2022-08-19 08:32:59 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ const App = () => (
         <Route path="/courses/:id/update" component={UpdateCourseWithContext} />
         <Route path="/courses/:id" component={CourseDetailWithContext} />
         <Route path="/signin" element={<UserSignInWithContext />} />
-        <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signup" element={<UserSignUpWithContext />} />
         <Route path="/signout" component={UserSignOutWithContext} />
       </Routes>
   </Router>
