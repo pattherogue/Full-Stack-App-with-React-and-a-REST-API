f2a34a0f498a9c46212d1d3e716ff375aea09019
Paolo Gomez <peweegardner248@gmail.com>
2022-08-30 12:17:57 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,12 +33,12 @@ const App = () => (
     <HeaderWithContext />
       <Switch>
         <Route exact path="/" component={CoursesWithContext} />
-        <PrivateRoute exact path="/courses/create" component={CreateCourseWithContext } />
-        <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext } />
-        <Route path="/courses/:id" component={CourseDetailWithContext } />
-        <Route path="/signin" component={UserSignInWithContext } />
-        <Route path="/signup" component={UserSignUpWithContext } />
-        <Route path="/signout" component={UserSignOutWithContext } />
+        <PrivateRoute exact path="/courses/create" component={CreateCourseWithContext} />
+        <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
+        <Route path="/courses/:id" component={CourseDetailWithContext} />
+        <Route path="/signin" component={UserSignInWithContext} />
+        <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signout" component={UserSignOutWithContext} />
       </Switch>
   </Router>
 
