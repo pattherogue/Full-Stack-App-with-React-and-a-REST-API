eaf75881d9c307980fc031fe5dd721fdf6fb95d8
Paolo Gomez <peweegardner248@gmail.com>
2022-09-01 10:36:30 -0700

    Update Context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Cookies from 'js-cookie';
 import Data from './Data';
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
 export class Provider extends Component {
     
