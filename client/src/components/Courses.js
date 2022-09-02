fd04ec74d9293b1a0d1af1ba47d1d03146937a55
Paolo Gomez <peweegardner248@gmail.com>
2022-09-02 11:04:05 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Courses() {
-    state = {
-        courses: []
-    };
+    const [ courses, setCourses ] = useState([]);
     // provide the "Courses" screen
     // retrieve list of courses from /api/courses
     useEffect(()=> {
