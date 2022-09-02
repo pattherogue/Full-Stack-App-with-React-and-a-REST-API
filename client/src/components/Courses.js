68899a63e72c37b6d6ef24831fadaf2f209cdeab
Paolo Gomez <peweegardner248@gmail.com>
2022-09-02 11:01:33 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -23,6 +23,8 @@ export default function Courses() {
 
     return (
         // render list of courses
+        <main>
+        <div className="wrap main--grid">
         {courses.map(course => {
             
             // link to respective "Course Detail" screen
@@ -35,16 +37,15 @@ export default function Courses() {
         })}
 
             // component render link to "Create Course" screen
-                <div className="wrap main--grid">
-                    {coursesList}
-                    <a class="course--module course--add--module" href="/courses/create">
+                    <Link class="course--module course--add--module" href="/courses/create">
                         <span class="course--add--title">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                             viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
                             New Course
                         </span>
-                    </a>
+                    </Link>
                 </div>
+        </main>
             
         
     );
