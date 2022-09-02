e27e8ff2b32b1e4b9d285576726fcb1c4db6a7a8
Paolo Gomez <peweegardner248@gmail.com>
2022-09-02 11:03:16 -0700

    Update Courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -24,27 +24,27 @@ export default function Courses() {
     return (
         // render list of courses
         <main>
-        <div className="wrap main--grid">
-        {courses.map(course => {
-            
-            // link to respective "Course Detail" screen
-            return (
-                <Link key={course.id} className="course--module course--link" href={`/courses/${course.id}`}>
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">{course.title}</h3>
-                </Link>
-            );
-        })}
+            <div className="wrap main--grid">
+                {courses.map(course => {
+                    
+                    // link to respective "Course Detail" screen
+                    return (
+                        <Link key={course.id} className="course--module course--link" href={`/courses/${course.id}`}>
+                            <h2 className="course--label">Course</h2>
+                            <h3 className="course--title">{course.title}</h3>
+                        </Link>
+                    );
+                })}
 
-            // component render link to "Create Course" screen
-                    <Link class="course--module course--add--module" href="/courses/create">
-                        <span class="course--add--title">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                            viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
-                            New Course
-                        </span>
-                    </Link>
-                </div>
+                 
+                <Link class="course--module course--add--module" href="/courses/create">
+                    <span class="course--add--title">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                        New Course
+                    </span>
+                </Link>
+            </div>
         </main>
             
         
