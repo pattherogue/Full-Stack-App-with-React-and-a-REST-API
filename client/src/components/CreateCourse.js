5179304e879248d838232101c828c02566221c52
Paolo Gomez <peweegardner248@gmail.com>
2022-09-14 20:25:41 -0700

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from 'react';
+import React, { Component, useContext, useState } from 'react';
 import Form from './Form';
 import { Context } from '../Context';
 import { useHistory } from "react-router-dom";
