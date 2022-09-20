fa34c9bc658dcae7632adf2b194f084c592845f6
Paolo Gomez <peweegardner248@gmail.com>
2022-09-20 00:34:41 -0700

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -12,7 +12,7 @@ export default function CourseDetail() {
     
     const [course, getCourse] = useState(
         {
-        
+            id: "",
         }
     );
     /*const [user, setUser] = useState({
