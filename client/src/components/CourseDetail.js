ebafc6543b07fb7d1ff40b3ab56b7525e9ffa015
Paolo Gomez <peweegardner248@gmail.com>
2022-09-23 19:02:17 -0700

    Update CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -59,7 +59,7 @@ export default function CourseDetail() {
                 }
             })
             .catch((err) => {
-                
+                console.log(err);
             })
     }
 
