62534fc2a80449bf4cb419d6fa4ed65655491109
Paolo Gomez <peweegardner248@gmail.com>
2022-10-11 09:55:14 -0700

    Update UserSignOut.js

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-import { Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 const UserSignOut = ({context}) => {
     // signs out autheticated user
     useEffect(() => context.actions.signOut());
     return (
         // redirects user to default route 
-        <Switch to="/" />
+        <Redirect to="/" />
     );
 }
 
