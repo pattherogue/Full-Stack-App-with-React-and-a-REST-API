06c393fc543c2a09600fe4a38ff7c348172f8b17
Paolo Gomez <peweegardner248@gmail.com>
2022-10-19 12:32:10 -0700

    Update users.js

--- api/models/users.js
+++ api/models/users.js
@@ -63,8 +63,10 @@ module.exports = (sequelize, DataTypes) =>  {
             allowNull: false,
 
             set(val) {
-                const hashedPassword = bcrypt.hashSync(val, 10);
-                this.setDataValue('password', hashedPassword);
+                if (val) {
+                    const hashedPassword = bcrypt.hashSync(val, 10);
+                    this.setDataValue('password', hashedPassword);
+                }
             },
         
             validate: {
